0672a0f70aea07139377b8a6d413aa7dfcab72de
Jack Doherty <doherty9@purdue.edu>
2020-09-26 23:16:58 -0400

    route url fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 const menus = require("./routes/api/v1/menus.js")
 
 //routes
-app.use("/routes/api/v1/menus", menus);
+app.use("/api/v1/menus", menus);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
